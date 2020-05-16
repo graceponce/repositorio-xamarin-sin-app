5540e02eeba45e6d4a9aeb8ee78300476d2ce131
graceponce9 <gracepatriciap93@gmail.com>
2020-05-16 13:39:25 -0600

    este es el primer commit del programa xamarin shop

---
+++ Shop/Shop.web/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.web.Models;
+
+namespace Shop.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Login()     
+        {
+            ViewData["Message"] = "Your login page.";
+            return View();
+        }
+        public IActionResult Carrito()
+        {
+            ViewData["Message"] = "Your Carrito page o Shop page.";
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            ViewData["Message"] = "Your Register data";
+            return View();
+        }
+        
+    }
+}
